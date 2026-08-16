34f36c8d86e784a859be71ca533479688da320dc
saminam <innam8264@gmail.com>
2026-08-16 17:46:07 +0530

    updated

--- src/app.js
+++ src/app.js
@@ -31,7 +31,8 @@ app.use(cors({
     const allowedOrigins = [
       'http://localhost:5173',
       'http://localhost:3000',
-      'https://invoice-datawyntechnologies.vercel.app'
+      'https://invoice-datawyntechnologies.vercel.app',
+      'https://invoice.datawyntechnologies.com'
     ]
     
     // Also add any origins from config
